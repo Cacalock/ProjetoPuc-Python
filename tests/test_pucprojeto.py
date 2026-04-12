25829335641142d2803400c7ac4bc627d25c0378
Cacalock <cassianebeatrizdarosa@hotmail.com>
2026-04-12 13:55:36 -0300

    V2.1: Mudanças no CI/CD

---
+++ tests/test_pucprojeto.py
@@ -0,0 +1,19 @@
+from pucprojeto import Operacoes
+
+def test_inicializacao():
+    op = Operacoes()
+    assert op is not None
+
+def test_lista_eh_dict():
+    op = Operacoes()
+    assert isinstance(op.lista, dict)
+
+def test_legenda_valida():
+    op = Operacoes()
+    assert op.legenda[1] == 'estudante(s)'
+    assert op.legenda[2] == 'professor(a/es)'
+
+def test_chaves_lista():
+    op = Operacoes()
+    # Garante que existem as 5 categorias
+    assert set(op.lista.keys()) == {1, 2, 3, 4, 5}
