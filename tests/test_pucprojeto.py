ca34fc45756376dfb5c7dd30e99859daa348100c
Cacalock <cassianebeatrizdarosa@hotmail.com>
2026-04-22 12:56:25 -0300

    V2.3: fix

--- tests/test_pucprojeto.py
+++ tests/test_pucprojeto.py
@@ -26,9 +26,9 @@ def test_chaves_lista():
 def test_processo_insercao():
     op = Operacoes()
     # Testa a inserção de um estudante
-    op.incluir(1, 'Alice')
+    op.incluir(1, 'Alice', 1231234)
     assert 'Alice' in op.lista[1]
 
     # Testa a inserção de um professor
-    op.incluir(2, 'Pedro Silva')
+    op.incluir(2, 'Pedro Silva', 123)
     assert 'Pedro Silva' in op.lista[2]
