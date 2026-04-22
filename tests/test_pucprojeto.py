ae5b44034a558fb6259697171eb6f04fe6f7adb1
Cacalock <cassianebeatrizdarosa@hotmail.com>
2026-04-22 12:31:38 -0300

    V2.2: Alertas e testes

--- tests/test_pucprojeto.py
+++ tests/test_pucprojeto.py
@@ -5,7 +5,6 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from pucprojeto import Operacoes
 
-from pucprojeto import Operacoes
 def test_inicializacao():
     op = Operacoes()
     assert op is not None
@@ -23,3 +22,9 @@ def test_chaves_lista():
     op = Operacoes()
     # Garante que existem as 5 categorias
     assert set(op.lista.keys()) == {1, 2, 3, 4, 5}
+
+def test_valores_iniciais():
+    op = Operacoes()
+    # Garante que todas as categorias começam com 0
+    for chave in op.lista:
+        assert op.lista[chave] == 0
